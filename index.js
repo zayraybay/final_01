4d60ac6abba6ff397cdbbd5fdbd21031c526648f
Ramon Torres <ramon.torres@kellogg.northwestern.edu>
2021-03-07 17:14:41 -0600

    HTML code by ramon on 03.07.21

--- index.js
+++ index.js
@@ -1,10 +1,31 @@
 firebase.auth().onAuthStateChanged(async function(user) {
+  
+  let db = firebase.firestore()
+
   if (user) {
     // Signed in
-    console.log('signed in')
+    console.log('user signed in')
+
+    db.collection('users').doc(user.uid).set({
+      email: user.email,
+      name: user.displayName
+  }) //closes db user auth
+
+  let currentUser = user.displayName
+  document.querySelector('.sign-in-or-sign-out').innerHTML = `
+  <div class="text-white-500 bold">Signed in as ${currentUser}</div>
+  <a href="#" class="sign-out text-purple-400 underline">Sign Out</a>
+  `
+  document.querySelector('.sign-out').addEventListener('click', function(event){
+      event.preventDefault()
+      firebase.auth().signOut()
+      document.location.href= "index.html"
+
+  }) //closes sign-out event listener
+
   } else {
     // Signed out
-    console.log('signed out')
+    console.log('user signed out')
 
     // Initializes FirebaseUI Auth
     let ui = new firebaseui.auth.AuthUI(firebase.auth())
